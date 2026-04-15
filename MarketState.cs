fa0225c1296dc6ab2ded4ef1859b2fb2f4161b29
Shidqi Akram Hauzan <57066616+Wafelkertas@users.noreply.github.com>
2026-04-15 14:44:16 +0700

    Add NPC needs-driven consumption system

--- MarketState.cs
+++ MarketState.cs
@@ -1,11 +1,13 @@
 namespace DynamicMarketEconomy;
 
+using System;
 using System.Collections.Generic;
 
 public class MarketState
 {
     public Dictionary<int, float> Demand { get; set; }
     public Dictionary<int, float> Supply { get; set; }
+    public Dictionary<string, NpcNeeds> Needs { get; set; }
     public Dictionary<int, float> RecentSales { get; set; }
     public Dictionary<int, float> SmoothedDemand { get; set; }
     public Dictionary<int, int> BasePriceByItem { get; set; }
@@ -15,6 +17,7 @@ public class MarketState
     {
         Demand = new Dictionary<int, float>();
         Supply = new Dictionary<int, float>();
+        Needs = new Dictionary<string, NpcNeeds>(StringComparer.OrdinalIgnoreCase);
         RecentSales = new Dictionary<int, float>();
         SmoothedDemand = new Dictionary<int, float>();
         BasePriceByItem = new Dictionary<int, int>();
---
+++ NpcNeeds.cs
@@ -0,0 +1,8 @@
+namespace DynamicMarketEconomy;
+
+public class NpcNeeds
+{
+    public float Hunger { get; set; }
+    public float Crafting { get; set; }
+    public float Luxury { get; set; }
+}
