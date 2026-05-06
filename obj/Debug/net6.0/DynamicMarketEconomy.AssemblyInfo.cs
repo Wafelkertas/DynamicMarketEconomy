dcb0832226fef5b9130337a7aba01ee8886e8aa2
Shidqi Akram Hauzan <57066616+Wafelkertas@users.noreply.github.com>
2026-05-06 17:54:02 +0700

    local commit
    local commit

--- obj/Debug/net6.0/DynamicMarketEconomy.AssemblyInfo.cs
+++ obj/Debug/net6.0/DynamicMarketEconomy.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DynamicMarketEconomy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dc2fa48914994c8167538d563ec02f186452f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DynamicMarketEconomy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DynamicMarketEconomy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
